c90312bf03fa726e5e51f691208ea52efd76964e
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2020-04-14 16:02:02 +0700

    Test skip ci [skip ci]

--- bin/create-trigger.sh
+++ bin/create-trigger.sh
@@ -1,8 +1,3 @@
-# gcloud beta builds triggers create cloud-source-repositories \
-#     --repo=django-example-app \
-#     --branch-pattern=".*" \
-#     --build-config="cloudbuild.yaml"
-
 gcloud beta builds triggers create github \
     --repo-name=django-example-app \
     --repo-owner=anhnguyen-agilityio \
